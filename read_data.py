509d220f8f6cb453bc0d0ba9004dc522d4e775f0
MMReisi <mohammad.mahdi.reisi2002@gmail.com>
2019-03-29 18:28:07 +0430

    add show function to read_data file to show voice with matplotlib

--- read_data.py
+++ read_data.py
@@ -1,10 +1,11 @@
 # here we want to read microphone or voice file
 import sounddevice as sd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class ReadData:
-    def __init__(self, fs=44100, channels=2):
+    def __init__(self, fs=44100, channels=1):
         self.fs = fs
         self.ch = channels
         self.duration = 5  # second
@@ -19,8 +20,17 @@ class ReadData:
         sd.play(self.voice, self.fs)
         sd.wait()
 
+    def show(self):
+        self.voice = self.voice.T.copy()
+        for i in range(len(self.voice)):
+            plt.plot(np.arange(len(self.voice[i])), self.voice[i]+i*5)
+        plt.show()
+
 
 if __name__ == '__main__':
     read = ReadData()
+    print('Recording')
     read.recording()
+    print('Play')
     read.play()
+    read.show()
