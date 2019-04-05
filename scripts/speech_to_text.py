5f2fe3e3c6bd7bde3a1c0cb78c05d908d9c56921
Parisa Mousavi <mousaviparisa5@yahoo.com>
2019-04-05 23:51:13 +0430

    add comments in order to get codes better

--- scripts/speech_to_text.py
+++ scripts/speech_to_text.py
@@ -1,5 +1,7 @@
+# import used modules
 import speech_recognition as sr
 from os import path
+
 
 class to_text():
     def __init__(self, name="english.wav",  audiofile="E:\\parisa\\projects\\voice_recognition\\voice-recognition-master\\wave_files\\english.wav", yousaid="error"):
@@ -7,11 +9,13 @@ class to_text():
         self.audiofile = audiofile
         self.yousaid = yousaid
     def read_audio(self):
+        '''reads path of audio file'''
         self.audiofile = path.join(path.dirname(path.realpath("E:\parisa\projects\\voice_recognition\\voice-recognition-master\\wave_files")), "english.wav")
     def print_text(self):
         r = sr.Recognizer()
-        with sr.AudioFile(self.audiofile) as source:
-            audio = r.record(source)
+        with sr.AudioFile(self.audiofile) as source:    # uses the audio file as the audio source
+            audio = r.record(source)    # read the entire audio file
+        # print the text of the audio
         self.yousaid = "You said: \n" + r.recognize_google(audio)
     def out_put(self):
         return self.yousaid
